bec082074a78cbdf37745bf960dec8d9f6f0bca0
Sergio Costa <sergiowoc@gmail.com>
2023-09-14 20:24:35 -0300

    styling the component.

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -40,6 +40,47 @@ class CardNews extends HTMLElement {
 
       return componentRoot;
     }
+
+    styles() {
+      const style = document.createElement("style");
+      style.textContent = `
+          .card{
+            width: 80%;
+            box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+            -webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+            -moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+          }
+          
+          
+          .card_left{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+          }
+          
+          .card_left > a {
+            margin-top: 15px;
+            font-size: 25px;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+          }
+          
+          .card_left > p {
+            color: rgb(70, 70, 70)
+          }
+          
+          .card_left > span {
+            font-weight: 400;
+          }
+      `;
+
+      return style;
+    }
   
 }
 
